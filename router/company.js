8a9c78e4d623aec45ff6906a44388e618cfa8b47
Zhangyu Dong <dzy303663@163.com>
2019-04-20 00:57:48 +0800

    学生申请岗位

--- router/company.js
+++ router/company.js
@@ -9,4 +9,21 @@ router.get('/company',(req,res) => {
         res.json(doc)
     });
 })
+
+router.post('/company/apply',(req,res) => {
+    console.log(req.body)
+    let temp;
+    let {company_id,selectedJob} = req.body;
+    company.findOne({user_id: company_id},(err,doc) => {
+        temp = {
+            selectedJob,
+            user: req.body,
+            createdTime: new Date().getTime()
+        };
+        console.log(doc)
+        doc.apply_list.push(temp);
+        doc.save();
+        res.json({msg: '操作成功'})
+    })
+})
 module.exports = router
